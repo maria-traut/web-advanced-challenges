b2135eefc2741a2019518007ea53f1260e60a78b
Maria Traut <mia.gotcha@proton.me>
2026-08-17 13:02:39 +0200

    start implementing comments service

--- nestJS/nestJS-basics_2/cyber-chat/src/threads/threads.module.ts
+++ nestJS/nestJS-basics_2/cyber-chat/src/threads/threads.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ThreadsService } from './threads.service';
-import { ThreadsController } from './threads.controller';
+import { Module } from "@nestjs/common";
+import { ThreadsService } from "./threads.service";
+import { ThreadsController } from "./threads.controller";
+import { CommentsService } from "../comments/comments.service";
 
 @Module({
-  providers: [ThreadsService],
-  controllers: [ThreadsController]
+  providers: [ThreadsService, CommentsService],
+  controllers: [ThreadsController],
 })
 export class ThreadsModule {}
