6fc2a0c14aa21d88d05974e5e867f740a56bb077
Maria Traut <mia.gotcha@proton.me>
2026-08-17 21:53:31 +0200

    change folder structure

---
+++ nestJS/nestJS-typeORM/cyber-chat/src/threads/threads.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  NotFoundException,
+  Delete,
+} from "@nestjs/common";
+import { ThreadsService } from "./threads.service";
+import { CommentsService } from "../comments/comments.service";
+import type { Thread, Comment } from "../types";
+
+@Controller("threads")
+export class ThreadsController {
+  constructor(
+    private readonly threadsService: ThreadsService,
+    private readonly commentsService: CommentsService,
+  ) {}
+
+  // GET    /threads   List all threads
+  @Get()
+  listAllThreads(): Thread[] {
+    return this.threadsService.getAllThreads();
+  }
+
+  // GET	/threads/:id	Get one thread including its comments
+  @Get(":id")
+  showThreadIncludingComments(
+    @Param("id") id: string,
+  ): Thread & { comments: Comment[] } {
+    const thread = this.threadsService.getThreadByIdIncludingComments(
+      Number(id),
+    );
+    if (!thread) {
+      throw new NotFoundException(`Thread with ID "${id}" not found.`);
+    }
+    return thread;
+  }
+
+  // POST	/threads	Create a thread with title and body
+  @Post()
+  createThread(
+    @Body() body: { title: string; author: string; body: string },
+  ): Thread {
+    return this.threadsService.addNewThread(body.title, body.author, body.body);
+  }
+
+  // POST	/threads/:id/comments	Add a comment to a thread
+  @Post(":id/comments")
+  createNewPost(
+    @Param("id") id: string,
+    @Body() body: { author: string; body: string },
+  ): Comment {
+    return this.commentsService.addNewComment(
+      Number(id),
+      body.author,
+      body.body,
+    );
+  }
+
+  // DELETE     /threads/:id/   Deletes the thread and all of its comments (comments are actually deleted)
+  @Delete(":id")
+  removeThread(@Param("id") id: string): { message: string } {
+    const deleted = this.threadsService.deleteThreadIncludingComments(
+      Number(id),
+    );
+    if (!deleted) {
+      throw new NotFoundException(`Thread with ID "${id}" not found.`);
+    }
+    return { message: `Thread with ID "${id}" deleted.` };
+  }
+}
