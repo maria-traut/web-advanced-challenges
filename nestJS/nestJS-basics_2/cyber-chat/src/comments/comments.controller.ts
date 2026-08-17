79d6a0f14817a65c660e4f5438a43f0ba102c587
Maria Traut <mia.gotcha@proton.me>
2026-08-17 12:15:22 +0200

    start implementing threads controller

--- nestJS/nestJS-basics_2/cyber-chat/src/comments/comments.controller.ts
+++ nestJS/nestJS-basics_2/cyber-chat/src/comments/comments.controller.ts
@@ -2,9 +2,6 @@ import { Controller } from "@nestjs/common";
 
 @Controller("comments")
 export class CommentsController {
-  // POST	/threads	Create a thread with title and body
-  // GET	/threads	List all threads
-  // GET	/threads/:id	Get one thread including its comments
-  // POST	/threads/:id/comments	Add a comment to a thread
-  // DELETE	/threads/:id/	Deletes the thread and all of its comments (comments are actually deleted)
+  // GET	/comments/:id/	Get one comment
+  // DELETE	/comments/:id/	Special: Does not delete the comment, but sets its body to “deleted”
 }
--- nestJS/nestJS-basics_2/cyber-chat/src/threads/threads.controller.ts
+++ nestJS/nestJS-basics_2/cyber-chat/src/threads/threads.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  NotFoundException,
+  Delete,
+} from "@nestjs/common";
+import { ThreadsService } from "./threads.service";
+import type { Thread } from "./entities/thread.type";
+import type { Comment } from "../comments/entities/comment.type";
 
-@Controller('threads')
-export class ThreadsController {}
+@Controller("threads")
+export class ThreadsController {
+  constructor(private readonly threadsService: ThreadsService) {}
+
+  // POST	/threads	Create a thread with title and body
+  @Post()
+  createThread(@Body() body: { title: string; body: string }): Thread {
+    return this.threadsService.addNewThread(body.title, body.body);
+  }
+
+  // GET	/threads	List all threads
+  @Get()
+  listAllThreads(): Thread[] {
+    return this.threadsService.getAllThreads();
+  }
+
+  // GET	/threads/:id	Get one thread including its comments
+  @Get(":id")
+  showThreadIncludingComments(
+    @Param("id") id: string,
+  ): Thread & { comments: Comment[] } {
+    const thread = this.threadsService.getThreadByIdIncludingComments(
+      Number(id),
+    );
+    if (!thread) {
+      throw new NotFoundException(`Thread with ID "${id}" not found.`);
+    }
+    return thread;
+  }
+
+  // POST	/threads/:id/comments	Add a comment to a thread
+  @Post(":id/comments")
+  createNewPost(
+    @Param("id") id: string,
+    @Body() body: { author: string; body: string },
+  ): Comment {
+    return this.threadsService.addNewComment(
+      Number(id),
+      body.author,
+      body.body,
+    );
+  }
+
+  // DELETE	/threads/:id/	Deletes the thread and all of its comments (comments are actually deleted)
+  @Delete(":id")
+  remove(@Param("id") id: string): { message: string } {
+    const deleted = this.threadsService.deleteThreadIncludingComments(
+      Number(id),
+    );
+    if (!deleted) {
+      throw new NotFoundException(`Thread with ID "${id}" not found.`);
+    }
+    return { message: `Thread with ID "${id}" deleted.` };
+  }
+}
