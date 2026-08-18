a007f0255c285314606c91352202c1cdee274a40
Maria Traut <mia.gotcha@proton.me>
2026-08-18 12:52:04 +0200

    adapt threads module

--- nestJS/nestJS-typeORM/cyber-chat/src/threads/threads.module.ts
+++ nestJS/nestJS-typeORM/cyber-chat/src/threads/threads.module.ts
@@ -1,14 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Thread } from "./entities/thread.entity";
+import { Comment } from "../comments/entities/comment.entity";
 import { ThreadsService } from "./threads.service";
 import { ThreadsController } from "./threads.controller";
-import { ThreadsRepository } from "./threads.repository";
-import { CommentsModule } from "../comments/comments.module";
 
 @Module({
-  providers: [ThreadsService, ThreadsRepository],
+  providers: [ThreadsService],
   controllers: [ThreadsController],
-  imports: [CommentsModule, TypeOrmModule.forFeature([Thread])],
+  imports: [TypeOrmModule.forFeature([Thread, Comment])],
 })
 export class ThreadsModule {}
