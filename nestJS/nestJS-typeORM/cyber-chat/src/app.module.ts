fb7d384504b0fcea2144281deb109619868d58af
Maria Traut <mia.gotcha@proton.me>
2026-08-18 12:42:57 +0200

    implement thread and comment entitiy

--- nestJS/nestJS-typeORM/cyber-chat/src/app.module.ts
+++ nestJS/nestJS-typeORM/cyber-chat/src/app.module.ts
@@ -1,4 +1,29 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CommentsModule } from "./comments/comments.module";
+import { ThreadsModule } from "./threads/threads.module";
+import { Thread } from "./threads/entities/thread.entity";
+import { Comment } from "./comments/entities/comment.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "better-sqlite3",
+      database: "data/cyberchat.sqlite",
+      entities: [Thread, Comment],
+      synchronize: true,
+      logging: false,
+      enableWAL: true,
+      statementCacheSize: 100,
+    }),
+    CommentsModule,
+    ThreadsModule,
+  ],
+})
+export class AppModule {}
+
+/*
+import { Module } from "@nestjs/common";
 import { CommentsModule } from "./comments/comments.module";
 import { ThreadsModule } from "./threads/threads.module";
 
@@ -8,3 +33,4 @@ import { ThreadsModule } from "./threads/threads.module";
   providers: [],
 })
 export class AppModule {}
+*/
