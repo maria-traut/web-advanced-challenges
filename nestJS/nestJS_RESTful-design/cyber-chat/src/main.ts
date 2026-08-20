b5f0fa3a3d81257e929d20c76a78254753dfef3d
Maria Traut <mia.gotcha@proton.me>
2026-08-20 21:49:48 +0200

    register class serializer interceptor

--- nestJS/nestJS_RESTful-design/cyber-chat/src/main.ts
+++ nestJS/nestJS_RESTful-design/cyber-chat/src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      excludeExtraneousValues: true,
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
