97d43554f97bc47b8b38c8c84c40b259843ac161
Maria Traut <mia.gotcha@proton.me>
2026-08-21 20:30:00 +0200

    adapt user response dto

--- nestJS/nestJS_auth/cyber-chat/src/users/dto/userResponse.dto.ts
+++ nestJS/nestJS_auth/cyber-chat/src/users/dto/userResponse.dto.ts
@@ -1,12 +1,9 @@
 import { Exclude, Expose } from "class-transformer";
 
-export class ThreadResponseDto {
+export class UserResponseDto {
   @Expose()
   id!: string;
 
   @Expose()
   username!: string;
-
-  @Exclude()
-  passwordHash!: string;
 }
--- nestJS/nestJS_auth/cyber-chat/src/users/users.controller.ts
+++ nestJS/nestJS_auth/cyber-chat/src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { User } from "./entity/user.entity";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findByUsername(username: string): Promise<User | null> {
+    return this.usersService.findByUsername(username);
+  }
+}
