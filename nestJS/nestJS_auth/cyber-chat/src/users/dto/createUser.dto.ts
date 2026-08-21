78dcc446e27fa761ec2a613d29196bc05386c178
Maria Traut <mia.gotcha@proton.me>
2026-08-21 16:34:57 +0200

    create user dto

---
+++ nestJS/nestJS_auth/cyber-chat/src/users/dto/createUser.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsNotEmpty, MaxLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  username!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  password!: string;
+}
--- nestJS/nestJS_auth/cyber-chat/src/users/entity/user.entity.ts
+++ nestJS/nestJS_auth/cyber-chat/src/users/entity/user.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from "bcrypt";
 
 @Entity("users")
 export class User {
@@ -11,3 +12,9 @@ export class User {
   @Column()
   passwordHash!: string;
 }
+
+// const saltOrRounds = 10;
+// const password = "random_password";
+// const hash = await bcrypt.hash(password, saltOrRounds);
+
+// const salt = await bcrypt.genSalt();
