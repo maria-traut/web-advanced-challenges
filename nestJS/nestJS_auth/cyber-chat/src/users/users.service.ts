53f01b6f267425235284b3154d92fe2e47766786
Maria Traut <mia.gotcha@proton.me>
2026-08-21 20:22:02 +0200

    write create user and find by unsername method

--- nestJS/nestJS_auth/cyber-chat/src/users/users.service.ts
+++ nestJS/nestJS_auth/cyber-chat/src/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entity/user.entity";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/createUser.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly users: Repository<User>,
+  ) {}
+
+  async createUser(dto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+    const passwordHash = await bcrypt.hash(dto.password, saltOrRounds);
+
+    const newUser = this.users.create({
+      username: dto.username,
+      passwordHash,
+    });
+    return this.users.save(newUser);
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    return this.users.findOneBy({ username });
+  }
+}
