21a2a883259eaf93039f9eefa412474ee846774e
Maria Traut <mia.gotcha@proton.me>
2026-08-21 12:11:07 +0200

    install jwt packages

---
+++ nestJS/nestJS_auth/cyber-chat/src/threads/entities/thread.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Comment } from "../../comments/entities/comment.entity";
+
+@Entity("threads")
+export class Thread {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ type: "varchar", length: 120 })
+  title!: string;
+
+  @Column({ type: "varchar", length: 120, nullable: true })
+  subtitle?: string;
+
+  @Column({ type: "text" })
+  body!: string;
+
+  @Column({ type: "varchar", length: 120 })
+  author!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  // One-to-many is a relation where B (comment) contains only one instance of A (thread)
+  // One thread can have many comments (inverse side of the relation)
+  @OneToMany(() => Comment, (comment) => comment.thread)
+  comments!: Comment[];
+}
---
+++ nestJS/nestJS_auth/cyber-chat/src/threads/threads.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Thread } from "./entities/thread.entity";
+import { Comment } from "../comments/entities/comment.entity";
+import { CreateThreadDto } from "./dto/createThread.dto";
+import { UpdateThreadDto } from "./dto/updateThread.dto";
+import { ThreadWithComments } from "../types";
+
+@Injectable()
+export class ThreadsService {
+  constructor(
+    @InjectRepository(Thread)
+    private readonly threads: Repository<Thread>,
+    @InjectRepository(Comment)
+    private readonly comments: Repository<Comment>,
+  ) {}
+
+  async create(dto: CreateThreadDto): Promise<Thread> {
+    const newThread = this.threads.create(dto);
+    return this.threads.save(newThread);
+  }
+
+  async findAll(): Promise<Thread[]> {
+    return this.threads.find();
+  }
+
+  async find(id: string): Promise<ThreadWithComments> {
+    const thread = await this.threads.findOneBy({ id });
+    if (!thread) {
+      throw new NotFoundException(`Thread with ID ${id} not found.`);
+    }
+    const comments = await this.comments.find({ where: { thread: { id } } });
+    return { ...thread, comments };
+  }
+
+  async update(id: string, dto: UpdateThreadDto): Promise<Thread | null> {
+    const thread = await this.threads.findOneBy({ id });
+    if (!thread) {
+      throw new NotFoundException(`Thread with ID '${id}' not found`);
+    }
+
+    Object.assign(thread, dto);
+    return this.threads.save(thread);
+  }
+
+  // or:
+  // async update(id: string, dto: UpdateThreadDto): Promise<Thread | null> {
+  //   await this.threads.update(id, dto);
+  //   return this.threads.findOneBy({ id });
+  // }
+
+  async delete(id: string): Promise<boolean> {
+    const comments = await this.comments.find({
+      where: {
+        thread: {
+          id,
+        },
+      },
+    });
+    await this.comments.remove(comments);
+    // delete() or remove(), what is the difference?
+    const result = await this.threads.delete(id);
+    return (result.affected ?? 0) > 0;
+  }
+}
