c8c1f1245da663b5139f2d410a51cb8c90792399
Maria Traut <mia.gotcha@proton.me>
2026-08-24 21:13:47 +0200

    add bruno folder

--- nestJS/nestJS_auth/cyber-chat/src/users/entity/user.entity.ts
+++ nestJS/nestJS_auth/cyber-chat/src/users/entity/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { Thread } from "../../threads/entities/thread.entity";
 import { Comment } from "../../comments/entities/comment.entity";
 
