faabb5ac3cd7f285fc2379bc77bc4d3ba4d20045
Maria Traut <mia.gotcha@proton.me>
2026-09-01 08:15:12 +0200

    delete folders

--- nestJS/nestJS_RESTful-design/cyber-chat/src/threads/dto/updateThread.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateThreadDto } from "./createThread.dto";
-
-export class UpdateThreadDto extends PartialType(CreateThreadDto) {}
--- nestJS/nestJS_auth/cyber-chat/src/threads/dto/threadWithCommentsResponse.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Expose, Type } from "class-transformer";
-import { ThreadResponseDto } from "./threadResponse.dto";
-import { CommentResponseDto } from "../../comments/dto/commentResponse.dto";
-
-export class ThreadWithCommentsResponseDto extends ThreadResponseDto {
-  @Expose()
-  @Type(() => CommentResponseDto)
-  comments!: CommentResponseDto[];
-}
